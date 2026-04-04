e00e5a3138624341b8026c70b4815e72a4f27994
Dylan <devegatorresdylan@gmail.com>
2026-04-04 17:09:59 -0500

    Crea todas las entidades completas

--- src/main/java/edu/unimagdalena/web/ceptu/entities/Category.java
+++ src/main/java/edu/unimagdalena/web/ceptu/entities/Category.java
@@ -1,4 +1,28 @@
 package edu.unimagdalena.web.ceptu.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
--- src/main/java/edu/unimagdalena/web/ceptu/entities/Inventory.java
+++ src/main/java/edu/unimagdalena/web/ceptu/entities/Inventory.java
@@ -1,4 +1,33 @@
 package edu.unimagdalena.web.ceptu.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "inventories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false, unique = true)
+    private Product product;
+
+    @Column(name = "available_stock", nullable = false)
+    private int availableStock;
+
+    @Column(name = "minimum_stock", nullable = false)
+    private int minimumStock;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 }
